dd791bbadcd878408bbc3883c23a9c7f93b4136c
Piotr Czeglik <piotr.czeglik@iohk.io>
2024-06-19 23:18:12 +0200

    chore: watch and publish

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,11 @@ import typescript from '@rollup/plugin-typescript';
 import svgr from '@svgr/rollup';
 import { vanillaExtractPlugin } from '@vanilla-extract/rollup-plugin';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import execute from 'rollup-plugin-shell';
 
 import packageJson from './package.json';
+
+const YALC_PUBLISH = process.env.YALC_PUBLISH === 'true';
 
 export default () => ({
   input: 'src/index.ts',
@@ -15,13 +18,19 @@ export default () => ({
       composite: false,
       exclude: ['**/*.stories.tsx'],
     }),
+    vanillaExtractPlugin({
+      identifiers: 'short',
+    }),
     peerDepsExternal(),
     commonjs(),
     image(),
     svgr({ icon: true }),
-    vanillaExtractPlugin({
-      identifiers: 'short',
-    }),
+    YALC_PUBLISH &&
+      execute({
+        commands: ['yalc push --no-pure'],
+        hook: 'closeBundle',
+        parallel: false,
+      }),
   ],
   output: [
     {
