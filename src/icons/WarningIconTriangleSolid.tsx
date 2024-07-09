0ba3e88658eb46f85e0db02ee86b010621d1372d
Tom Mayel <tom.mayel@iohk.io>
2024-07-09 14:04:39 +0100

    feat(icons): add warning triangle icon solid

---
+++ src/icons/WarningIconTriangleSolid.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const SvgWarningIconTriangleSolid = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M9.701 4.344c1.009-1.792 3.59-1.792 4.598 0l7.358 13.083c.99 1.758-.281 3.93-2.298 3.93H4.64c-2.017 0-3.287-2.172-2.298-3.93zm3.618 13.057a1.319 1.319 0 1 1-2.638 0 1.319 1.319 0 0 1 2.638 0m-1.32-10.55c-.727 0-1.318.59-1.318 1.32v3.955a1.319 1.319 0 1 0 2.638 0V8.17c0-.728-.59-1.319-1.32-1.319"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgWarningIconTriangleSolid;
