2bf8bae2a5f4e85d33139c108c30c50e91cdb5c2
Tom Mayel <tom.mayel@iohk.io>
2024-07-11 12:18:33 +0100

    fix(icons): fix warning icon's name

--- src/icons/WarningIconTriangleSolidComponent.tsx
+++ src/icons/WarningIconTriangleSolidComponent.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import type { SVGProps } from 'react';
-const SvgWarningIconTriangleSolid = (props: SVGProps<SVGSVGElement>) => (
+const SvgWarningIconTriangleSolidcomponent = (
+  props: SVGProps<SVGSVGElement>,
+) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={24}
@@ -16,4 +18,4 @@ const SvgWarningIconTriangleSolid = (props: SVGProps<SVGSVGElement>) => (
     />
   </svg>
 );
-export default SvgWarningIconTriangleSolid;
+export default SvgWarningIconTriangleSolidcomponent;
